0c24d9a0b11cb0bf54dd5eaf6dec5f54375e860f
Pietro Ferrara <pietro.ferrara@unive.it>
2021-04-16 10:24:37 +0200

    Lezione 20: dump and project

--- Lezione17_CsvReader/file_processor.cpp
+++ Lezione17_CsvReader/file_processor.cpp
@@ -3,8 +3,13 @@
 #include <string>
 #include <sstream>
 #include <fstream>
+#include <algorithm>
 
 using namespace std;
+
+enum column_types {
+    date, number, string_type
+};
 
 struct file_error {
     string message;
@@ -44,6 +49,10 @@ class Date {
             day = d;
         }
 
+        string to_string() {
+            return std::to_string(day)+"\\"+std::to_string(month)+"\\"+std::to_string(year);
+        }
+
     private:
         int day, month, year;
 };
@@ -54,39 +63,120 @@ class Table {
             ifstream file{filepath};
 
             if(file.good()) {
-                string headers_row;
+                string headers_row; 
                 getline(file, headers_row, '\n');
                 headers = parse_line(headers_row);
             }
             else throw new file_error{"Unable to read properly "+filepath};
-            data = vector<vector<string>>{headers.size()};
+            vector<vector<string>> data = vector<vector<string>>{headers.size()};
             while(file.good()) {
                 string s;
                 getline(file, s, '\n');
-                vector<string> line_values = parse_line(s);
-                for(int i = 0; i < line_values.size(); i++)
-                    data.at(i).push_back(line_values.at(i));
+                if(! s.empty()) {
+                    vector<string> line_values = parse_line(s);
+                    for(int i = 0; i < line_values.size(); i++)
+                        data.at(i).push_back(line_values.at(i));
+                }
             }
             if(! file.eof())
                 throw new file_error{"Error while reading file "+filepath};
 
             for(int i = 0; i < data.size(); i++) {
                 vector<string> v = data.at(i);
-                if(isDate(v))
+                if(isDate(v)) {
+                    col_types.push_back(date);
                     date_columns.push_back(transform_to_dates(v));
-                /*else if(isNumerical(v))
+                }
+                else if(isNumerical(v)){
+                    col_types.push_back(number);
                     numerical_columns.push_back(transform_to_numerical(v));
-                else string_columns.push_back(v); */
+                }
+                else {
+                    col_types.push_back(string_type);
+                    string_columns.push_back(v);
+                }
             }
         }
 
-    private:
-        vector<string> headers;
-        vector<vector<string>> data;
+        Table project(string column_string, vector<string> columns_numerical) {
+            int index_column_string = transform_index_to_string_columns(get_column_index(column_string));
+            vector<int> index_columns_numerical;
+            for(string c : columns_numerical)
+                index_columns_numerical.push_back(transform_index_to_numerical_columns(get_column_index(c)));
+            vector<column_types> types;
+            vector<string> headers;
+            vector<string> v_cs = string_columns.at(index_column_string);
+            types.push_back(string_type);
+            headers.push_back(column_string);
+            for(string s : columns_numerical)
+                headers.push_back(s);
+            vector<vector<int>> v_numbers;
+            for(int index : index_columns_numerical) {
+                types.push_back(number);
+                v_numbers.push_back(numerical_columns.at(index));
+            }
 
+            vector<vector<Date>> dates;
+            vector<vector<string>> s;
+            s.push_back(v_cs);
+
+
+            Table result{types, headers, dates, v_numbers, s};
+            return result;
+
+        }
+
+
+        void dump_to_file(string file_path) {
+            ofstream output{file_path};
+            if(!output.good())
+                throw file_error{"The output file "+file_path+" cannot be opened"};
+            for(int i = 0; i < headers.size(); i++) {
+                output << headers.at(i);
+                if(i!=headers.size()-1)
+                    output << ",";
+            }
+            output << "\n";
+            int number_of_rows = string_columns.at(0).size();
+            for(int row = 0; row < number_of_rows; row++) {
+                for(int column = 0; column < col_types.size(); column++) {
+                    if(col_types.at(column)==date) {
+                        int date_index = transform_index_to_date_columns(column);
+                        output << date_columns.at(date_index).at(row).to_string();
+                    }
+                    else if(col_types.at(column)==number) {
+                        int numerical_index = transform_index_to_numerical_columns(column);
+                        output << numerical_columns.at(numerical_index).at(row);
+                    }
+                    else {
+                        int string_index = transform_index_to_string_columns(column);
+                        output << string_columns.at(string_index).at(row);
+                    }
+
+                    if(column!=col_types.size()-1)
+                        output << ",";
+                }
+                output << "\n";
+            }
+            output.close();
+        }
+
+    private:
+        vector<column_types> col_types;
+
+        vector<string> headers;
+        
         vector<vector<Date>> date_columns;
-        vector<vector<double>> numerical_columns;
+        vector<vector<int>> numerical_columns;
         vector<vector<string>> string_columns;
+
+        Table(vector<column_types> t, vector<string> h, vector<vector<Date>> d, vector<vector<int>> n, vector<vector<string>> s) {
+            col_types = t;
+            headers = h;
+            date_columns = d;
+            numerical_columns = n;
+            string_columns = s;
+        }
 
         vector<string> parse_line(string row) {
                 istringstream single_header{row};
@@ -98,6 +188,67 @@ class Table {
                 }
                 return result;
         }
+
+        int transform_index_to_string_columns(int index) {
+            int result = 0;
+            for(int i = 0; i < index; i++)
+                if(col_types.at(i)==string_type)
+                    result++;
+            return result;
+        }
+
+
+        int transform_index_to_date_columns(int index) {
+            int result = 0;
+            for(int i = 0; i < index; i++)
+                if(col_types.at(i)==date)
+                    result++;
+            return result;
+        }
+
+        int transform_index_to_numerical_columns(int index) {
+            int result = 0;
+            for(int i = 0; i < index; i++)
+                if(col_types.at(i)==number)
+                    result++;
+            return result;
+        }
+
+        int get_column_index(string header) {
+            auto it_cs = find(headers.begin(), headers.end(), header);
+            if(it_cs == headers.end())
+                throw format_error{"Column "+header+" is not part of the table"};
+            return it_cs-headers.begin();
+        }
+        bool isNumerical(vector<string> v) {
+
+            string value = v.at(0);
+            try {
+                transform_value_to_number(value);
+                return true;
+            }
+            catch(format_error e) {
+                return false;
+            }
+        }
+
+        vector<int> transform_to_numerical(vector<string> values) {
+            vector<int> result;
+            for(string v : values)
+                result.push_back(transform_value_to_number(v));
+            return result;
+        }
+
+        int transform_value_to_number(string v) {
+            istringstream s{v};
+            int value;
+            s >> value;
+            if(! s.eof())
+                throw format_error{"String "+v+" is not a numerical value"};
+            else
+                return value;
+        }
+
 
         bool isDate(vector<string> v) {
             string value = v.at(0);
@@ -123,5 +274,14 @@ class Table {
 };
 
 int main() {
-    Table table{"/home/prog2021/labprog/LabProg_2021/Lezione17_CsvReader/somministrazioni-vaccini-summary-latest.csv"};    return 0;
+    Table table{"/home/prog2021/labprog/LabProg_2021/Lezione17_CsvReader/somministrazioni-vaccini-summary-latest.csv"};
+    
+    
+    vector<string> numericals;
+    numericals.push_back("totale");
+    numericals.push_back("categoria_over80");
+    Table result=table.project("nome_area", numericals);
+    result.dump_to_file("/home/prog2021/labprog/LabProg_2021/Lezione17_CsvReader/our_somministrazioni-vaccini-summary-latest.csv");
+    
+    return 0;
 }
